1580a59db1133637f52b89a9be6425cb5f4f2480
Kyo Nagashima <kyo@hail2u.net>
2009-04-01 01:15:02 +0900

    initial commit

---
+++ chrome/content/context_style_switcher_overlay.js
@@ -0,0 +1,21 @@
+var contextStyleSwitcher = {
+  init: function() {
+    var contextMenu = document.getElementById("contentAreaContextMenu");
+    contextMenu.addEventListener("popupshowing", contextStyleSwitcher.toggleVisibility, false);
+  },
+
+  toggleVisibility: function() {
+    if (gContextMenu.onTextInput
+      || gContextMenu.onImage
+      || gContextMenu.onLink
+      || gContextMenu.isTextSelected) {
+      document.getElementById("context-style-switcher_sep").hidden = true;
+      document.getElementById("context-style-switcher_menu").hidden = true;
+    } else {
+      document.getElementById("context-style-switcher_sep").hidden = false;
+      document.getElementById("context-style-switcher_menu").hidden = false;
+    }
+  }
+}
+
+window.addEventListener("load", contextStyleSwitcher.init, false);
